af13ebb1361e6aece0ea4659c0c02af5199e799e
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-01-16 18:42:03 -0300

    Create doWhileCsharp.cs

---
+++ doWhileCsharp.cs
@@ -0,0 +1,32 @@
+
+Random random = new Random();
+
+int lifeHero = 10;
+int lifeMonster = 10;
+
+do
+{
+    int roll = random.Next(1, 11);
+    lifeMonster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {lifeMonster} health.");
+
+    if (lifeMonster <= 0)
+    {
+        Console.WriteLine("Hero Wins!");
+        break;
+    }
+
+    roll = random.Next(1, 11);
+    lifeHero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {lifeHero} health.");
+
+    if (lifeMonster <= 0)
+    {
+        Console.WriteLine("Hero Wins!");
+        break;
+    }
+
+} while(lifeHero > 0 && lifeMonster > 0);
+Console.WriteLine(lifeHero > lifeMonster ? "Hero wins!" : "Monster wins!");
+
+
